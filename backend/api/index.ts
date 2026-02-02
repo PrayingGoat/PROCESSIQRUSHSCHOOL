48669da2dd21433c13fc891e0660ed691430991e
NyAina11 <rakotonirinanyaina11@gmail.com>
2026-02-02 20:57:27 +0300

    login et maj du formulaire 2

---
+++ backend/api/index.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+let cachedApp: any;
+
+export default async function handler(req: any, res: any) {
+    if (!cachedApp) {
+        const app = await NestFactory.create(AppModule);
+        app.setGlobalPrefix('api');
+        app.enableCors();
+        app.useGlobalPipes(new ValidationPipe());
+        await app.init();
+        cachedApp = app.getHttpAdapter().getInstance();
+    }
+    return cachedApp(req, res);
+}
